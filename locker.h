681634ed261ea29d706fecd70ee5f1ca01c38638
04rk5k1n <1479695027@qq.com>
2023-04-18 21:31:52 +0800

    线程同步控制类实现

--- locker.h
+++ locker.h
@@ -7,8 +7,25 @@
 
 //线程同步机制封装类
 //互斥锁类
-class locker{
-    
+
+class locker {
+public:
+    locker() {
+        if(pthread_mutex_init(&m_mutex, NULL) != 0) {
+            throw std::exception();
+        }
+    }
+
+    ~locker() {
+        phread_mutex_destroy(&m_mutex, NULL);
+    }
+
+    bool lock() {
+        return phread_mutex_lock
+    }
+
+private:
+    pthread_mutex_t m_mutex;
 }
 
 
